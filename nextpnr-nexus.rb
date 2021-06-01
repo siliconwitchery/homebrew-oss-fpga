3e4b6bb6bee06ade37e646024398a11fb771209e
Raj Nakarja <raj@siliconwitchery.com>
2021-06-01 11:37:42 +0200

    added initial nexus script

---
+++ nextpnr-nexus.rb
@@ -0,0 +1,22 @@
+class NextpnrNexus < Formula
+    desc "portable FPGA place-and-route tool"
+    homepage "https://github.com/YosysHQ/nextpnr"
+    head "https://github.com/YosysHQ/nextpnr.git"
+  
+    depends_on "cmake" => :build
+    depends_on "pkg-config" => :build
+    depends_on "eigen" => :build
+    depends_on "python@3.9"
+    depends_on "boost"
+    depends_on "boost-python3"
+    depends_on "qt5"
+    # depends_on "project-oxide"
+  
+    def install
+      system "cmake", "-DARCH=nexus", "-DOXIDE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}", \
+         ".", *std_cmake_args, "-DBoost_NO_BOOST_CMAKE=on", "-DBUILD_TESTS=OFF"
+      system "make", "install"
+    end
+  
+  end
+  
